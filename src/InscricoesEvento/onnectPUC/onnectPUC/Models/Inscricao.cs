77975464935a157170c3d42649fbfaa8abe46a73
Derick Lucas Alves Rodrigues <124528221+DerickLRodrigues@users.noreply.github.com>
2025-06-11 22:01:36 -0300

    attId

--- src/InscricoesEvento/onnectPUC/onnectPUC/Models/Inscricao.cs
+++ src/InscricoesEvento/onnectPUC/onnectPUC/Models/Inscricao.cs
@@ -4,8 +4,8 @@ namespace InscricaoService.Models
     public class Inscricao
     {
         public int Id { get; set; }
-        public Guid EventoId { get; set; }
-        public Guid UsuarioId { get; set; }
+        public int EventoId { get; set; }
+        public int UsuarioId { get; set; }
         public DateTime DataInscricao { get; set; } = DateTime.UtcNow;
         public bool Ativa { get; set; } = true;
     }
